2f1844008ae9806ba1e592a7e945801399fc4973
Leitino <alessandro.leita89@gmail.com>
2018-03-23 18:07:53 +0100

    avanzamento

--- jsnacksBar10.js
+++ jsnacksBar10.js
@@ -1,28 +1,33 @@
 var film1 = {
   nome : 'ciao ciao',
   attori:['qui', 'quo', 'qua'],
-  durata:120,
+  durata: 120,
   voto:[8, 7, 6],
   genere:'horror',
 }
 var film2 = {
   nome : 'mr & mrs smith',
   attori:['Brad Pitt', 'Angelina Jolie'],
-  durata:130,
+  durata: 130,
   voto:[7, 8, 7],
   genere:'drammatico',
 }
 var film3 = {
   nome : 'arrivedorci',
   attori:['stalio', 'olio'],
-  durata:100,
+  durata: 100,
   voto:[9, 6, 7],
   genere:'comico',
 }
 
-var listaFilm=['film1', 'film2', 'film3']
+var listaFilm=[film1,film2,film3]
 
 var sommaDurata=0
+var mediaDurata=0
 for (var i = 0; i < listaFilm.length; i++) {
-  console.log(listaFilm[i].durata)
+  sommaDurata=sommaDurata+listaFilm[i].durata
+  mediaDurata=sommaDurata/listaFilm[i].durata
 }
+
+console.log(sommaDurata)
+console.log(mediaDurata)
