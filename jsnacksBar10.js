935faa8cf4e957df1ad46d539efd767e6de3e95c
Leitino <alessandro.leita89@gmail.com>
2018-03-23 17:39:29 +0100

    esercizio 10 avanzando

--- jsnacksBar10.js
+++ jsnacksBar10.js
@@ -1,7 +1,28 @@
-function film(nome, [attori], durataMin, [voti], genere) {
-  this.nome=nome
-  this.attori=attori
-  this.durataMin=durataMin
-  this.voti=voti
-  this.genere=genere
+var film1 = {
+  nome : 'ciao ciao',
+  attori:['qui', 'quo', 'qua'],
+  durata:120,
+  voto:[8, 7, 6],
+  genere:'horror',
+}
+var film2 = {
+  nome : 'mr & mrs smith',
+  attori:['Brad Pitt', 'Angelina Jolie'],
+  durata:130,
+  voto:[7, 8, 7],
+  genere:'drammatico',
+}
+var film3 = {
+  nome : 'arrivedorci',
+  attori:['stalio', 'olio'],
+  durata:100,
+  voto:[9, 6, 7],
+  genere:'comico',
+}
+
+var listaFilm=['film1', 'film2', 'film3']
+
+var sommaDurata=0
+for (var i = 0; i < listaFilm.length; i++) {
+  console.log(listaFilm[i].durata)
 }
