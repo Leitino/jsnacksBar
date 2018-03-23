a491d6788c9af7887adb4f52a55ae611c6b79b0b
Leitino <alessandro.leita89@gmail.com>
2018-03-23 13:31:27 +0100

    esercizio 7 finito

---
+++ jsnacksBar7.js
@@ -0,0 +1,7 @@
+var invitati=['Marco Rossi', 'Giulio Rossi', 'Fausto Rossi','Marta Verdi', 'Sonia Verdi', 'Federica Verdi']
+var invitatiUomo=['Marco', 'Giulio', 'Fausto']
+var invitatiDonna=['Marta', 'Sonia', 'Federica']
+console.log(invitati)
+console.log(invitatiUomo)
+console.log(invitatiDonna)
+alert('gli invitati uomo sono il ' + invitatiUomo.length/invitati.length*100 +'%' + ' e le invitate donna sono il ' + invitatiDonna.length/invitati.length*100 + '%')
