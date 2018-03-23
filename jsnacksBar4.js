d835e08ac1208a0c411b4a7a1df02abbd4331233
Leitino <alessandro.leita89@gmail.com>
2018-03-23 11:20:23 +0100

    sercizio 4 con do while finito

--- jsnacksBar4.js
+++ jsnacksBar4.js
@@ -1,7 +1,15 @@
 var somma=0
 
-for (var i = 0; i < 5; i++) {
+/*for (var i = 0; i < 5; i++) {
   var numUtente=parseInt(prompt('inserisci numero'))
   somma=(somma+numUtente)
 }
-  console.log(somma)
+  console.log(somma)*/
+
+contatore=0
+do {
+  var nomeUtente=parseInt(prompt('inserisci numero'))
+  somma=somma+nomeUtente
+  contatore++
+} while (contatore<=5);
+console.log(somma)
