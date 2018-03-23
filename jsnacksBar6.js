316b166c3ba2ddf1f4e1f1abb22dbf4b0985786b
Leitino <alessandro.leita89@gmail.com>
2018-03-23 13:11:51 +0100

    esercizio 6 finito

---
+++ jsnacksBar6.js
@@ -0,0 +1,9 @@
+var invitati=['Fabio', 'Michele', 'Alessandro', 'Samuel']
+
+var richiesta=prompt('il tuo nome')
+if (invitati.includes(richiesta)) {
+  alert('sei dei nostri puoi entrare')
+}
+else {
+  alert('accesso negato')
+}
